f8f77b047027330e4b7f26e520a0cbb3c04f2e7c
Andrew Tate <tate@gmail.com>
2022-09-12 00:30:03 +0300

    fix kek 3

--- src/app/components/footer/footer.component.jsx
+++ src/app/components/footer/footer.component.jsx
@@ -23,9 +23,31 @@ const Footer = () => {
               </div>
               <Social />
             </div>
-            {/* <div className="footer__right">
-              menu
-            </div> */}
+            <div className="footer__right">
+              <div className="footer__menu">
+                <div className="footer__menu-item"><div className="footer__divider"></div><a href="#">Home</a></div>
+                <div className="footer__menu-item"><a href="#">Staking</a></div>
+                <div className="footer__menu-item"><a href="#">Key Features</a></div>
+                <div className="footer__menu-item"><a href="#">Roadmap</a></div>
+                <div className="footer__menu-item"><a href="#">Partners</a></div>
+              </div>
+              <div className="footer__menu">
+                <div className="footer__menu-item"><div className="footer__divider"></div><a href="#">Whitepaper</a></div>
+                <div className="footer__menu-item"><a href="#">What is CoinFlip?</a></div>
+                <div className="footer__menu-item"><a href="#">Launch date</a></div>
+                <div className="footer__menu-item"><a href="#">Roadmap</a></div>
+                <div className="footer__menu-item"><a href="#">Partners</a></div>
+              </div>
+              <div className="footer__menu">
+                <div className="footer__menu">
+                  <div className="footer__menu-item"><div className="footer__divider"></div><a href="#">$CFC Token</a></div>
+                  <div className="footer__menu-item"><a href="#">PancakeSwap</a></div>
+                  <div className="footer__menu-item"><a href="#">PinkSale</a></div>
+                  <div className="footer__menu-item"><a href="#">CoinMarketCap</a></div>
+                  <div className="footer__menu-item"><a href="#">CoinGecko</a></div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='footer__legal'>
             <p className='footer__legal-rights'>
