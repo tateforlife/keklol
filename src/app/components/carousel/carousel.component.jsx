e7f778767d7a9db8eb43562d16b853a8d9520805
Andrew Tate <tate@gmail.com>
2022-09-11 17:53:16 +0300

    12345

--- src/app/components/carousel/carousel.component.jsx
+++ src/app/components/carousel/carousel.component.jsx
@@ -63,7 +63,7 @@ const Carousel = () => {
                       </linearGradient>
                     </defs>
                   </svg>
-              Platform
+              Staking
                 </h4>
                 <a className='carousel__link' href='#'>
               Read more in the Whitepaper
@@ -73,14 +73,13 @@ const Carousel = () => {
                   <div className="carousel__divider"></div>
                 </a>
                 <p className="carousel__paragraph">
-                  <span>Leaderboard pool of rewards.</span> Played once a week among the most active participants of the project, a jackpot that allows you to multiply your investments 10 times in just 5 minutes and much more.
-                </p>
+                  <span>New generation web 3.0 stacking.</span> Allows any holder of $CFC earn more tokens by staking it for a certain time.                </p>
               </div>
             </div>
           </div>
           <div>
             <div className='carousel__item'>
-              <img className='carouseL__img' src='carousel_2.png' alt='carousel 1' />
+              <img className='carouseL__img' src='carousel_3.png' alt='carousel 1' />
               <div className="carousel__content">
                 <h4 className='carousel__heading-small'>
                   <svg className='carousel__svg' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -92,7 +91,7 @@ const Carousel = () => {
                       </linearGradient>
                     </defs>
                   </svg>
-              Platform
+              Rewards
                 </h4>
                 <a className='carousel__link' href='#'>
               Read more in the Whitepaper
@@ -102,14 +101,13 @@ const Carousel = () => {
                   <div className="carousel__divider"></div>
                 </a>
                 <p className="carousel__paragraph">
-                  <span>Leaderboard pool of rewards.</span> Played once a week among the most active participants of the project, a jackpot that allows you to multiply your investments 10 times in just 5 minutes and much more.
-                </p>
+                  <span>Leaderboard pool of rewards.</span> Played once a week among the most active participants of the project, a jackpot that allows you to multiply your investments 10 times in just 5 minutes and much more.                 </p>
               </div>
             </div>
           </div>
           <div>
             <div className='carousel__item'>
-              <img className='carouseL__img' src='carousel_2.png' alt='carousel 1' />
+              <img className='carouseL__img' src='carousel_4.png' alt='carousel 1' />
               <div className="carousel__content">
                 <h4 className='carousel__heading-small'>
                   <svg className='carousel__svg' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -121,7 +119,7 @@ const Carousel = () => {
                       </linearGradient>
                     </defs>
                   </svg>
-              Platform
+                  Token Allocation
                 </h4>
                 <a className='carousel__link' href='#'>
               Read more in the Whitepaper
@@ -131,8 +129,35 @@ const Carousel = () => {
                   <div className="carousel__divider"></div>
                 </a>
                 <p className="carousel__paragraph">
-                  <span>Leaderboard pool of rewards.</span> Played once a week among the most active participants of the project, a jackpot that allows you to multiply your investments 10 times in just 5 minutes and much more.
-                </p>
+                  <span>Transparent and honest token sharing.</span> Described in detail in whitepaper, available for viewing to any user.                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className='carousel__item'>
+              <img className='carouseL__img' src='carousel_5.png' alt='carousel 1' />
+              <div className="carousel__content">
+                <h4 className='carousel__heading-small'>
+                  <svg className='carousel__svg' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="7.5" cy="7.5" r="6" stroke="url(#paint0_linear_435_970)" strokeWidth="3"/>
+                    <defs>
+                      <linearGradient id="paint0_linear_435_970" x1="15" y1="7.49995" x2="4.98441e-08" y2="7.49995" gradientUnits="userSpaceOnUse">
+                        <stop stopColor="#9960FF"/>
+                        <stop offset="1" stopColor="#19D6E2"/>
+                      </linearGradient>
+                    </defs>
+                  </svg>
+              SAFU
+                </h4>
+                <a className='carousel__link' href='#'>
+              Read more in the Whitepaper
+                  <svg className='more-arrow' width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.7281 8.70711C9.11863 8.31658 9.11863 7.68342 8.7281 7.29289L2.36414 0.928932C1.97362 0.538408 1.34045 0.538408 0.949928 0.928932C0.559404 1.31946 0.559404 1.95262 0.949928 2.34315L6.60678 8L0.949928 13.6569C0.559404 14.0474 0.559404 14.6805 0.949928 15.0711C1.34045 15.4616 1.97362 15.4616 2.36414 15.0711L8.7281 8.70711ZM7.021 9H8.021V7H7.021V9Z" fill="white"/>
+                  </svg>
+                  <div className="carousel__divider"></div>
+                </a>
+                <p className="carousel__paragraph">
+                  <span>Verified by the pink sale platform and received the SAFU ticker.</span> CoinFlip is an absolutely safe project for investment.                </p>
               </div>
             </div>
           </div>
