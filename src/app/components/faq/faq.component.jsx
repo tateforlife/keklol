49c591cfcc586199e63bf1875ee021dda9e222b2
Andrew Tate <tate@gmail.com>
2022-09-18 12:11:50 +0300

    fix kek 91

--- src/app/components/faq/faq.component.jsx
+++ src/app/components/faq/faq.component.jsx
@@ -25,26 +25,27 @@ const FAQ = () => {
           </radialGradient>
         </defs>
       </svg>
-      <svg className='faq__back-2' width="1440" height="1830" viewBox="0 0 1440 1830" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_f_500_3492)">
-          <ellipse cx="720" cy="914.703" rx="720" ry="715.703" fill="url(#paint0_radial_500_3492)" fillOpacity="0.5"/>
-          <ellipse cx="846.048" cy="801.547" rx="290.292" ry="288.382" fill="#81DEC2"/>
-          <ellipse cx="635.968" cy="801.547" rx="290.292" ry="288.382" fill="#AE7DE4"/>
-          <ellipse cx="734.324" cy="986.799" rx="317.984" ry="341.857" fill="#58C0C6"/>
-          <ellipse cx="835.544" cy="1089.93" rx="290.292" ry="288.382" fill="#C37FF5"/>
+      <svg className='faq__back-2' width="1440" height="1066" viewBox="0 0 1440 1066" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_f_604_731)">
+          <ellipse cx="720" cy="715.703" rx="720" ry="715.703" fill="url(#paint0_radial_604_731)" fillOpacity="0.5"/>
+          <ellipse cx="846.048" cy="602.547" rx="290.292" ry="288.382" fill="#81DEC2"/>
+          <ellipse cx="635.968" cy="602.547" rx="290.292" ry="288.382" fill="#AE7DE4"/>
+          <ellipse cx="734.324" cy="787.799" rx="317.984" ry="341.857" fill="#58C0C6"/>
+          <ellipse cx="835.544" cy="890.928" rx="290.292" ry="288.382" fill="#C37FF5"/>
         </g>
         <defs>
-          <filter id="filter0_f_500_3492" x="-199" y="0" width="1838" height="1829.41" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <filter id="filter0_f_604_731" x="-199" y="-199" width="1838" height="1829.41" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
             <feFlood floodOpacity="0" result="BackgroundImageFix"/>
             <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-            <feGaussianBlur stdDeviation="99.5" result="effect1_foregroundBlur_500_3492"/>
+            <feGaussianBlur stdDeviation="99.5" result="effect1_foregroundBlur_604_731"/>
           </filter>
-          <radialGradient id="paint0_radial_500_3492" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(720 914.703) rotate(90) scale(466.485 469.286)">
+          <radialGradient id="paint0_radial_604_731" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(720 715.703) rotate(90) scale(466.485 469.286)">
             <stop stopColor="white"/>
             <stop offset="1" stopColor="#202742" stopOpacity="0"/>
           </radialGradient>
         </defs>
       </svg>
+
       <span id="faq"></span>
       <svg width="42" height="56" viewBox="0 0 42 56" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M40.8438 52.5L36.9375 48.5938C38.9688 45.625 38.6563 41.4062 36 38.75C34.4375 37.3437 32.4062 36.5625 30.375 36.5625C28.3438 36.5625 26.3125 37.3437 24.9062 38.9062C21.9375 41.875 21.9375 46.875 24.9062 50C26.3125 51.4062 28.3438 52.3437 30.375 52.3437C31.9375 52.3437 33.3437 51.875 34.75 51.0937L38.6562 55C39.2812 55.625 40.2188 55.625 40.8438 55C41.3125 54.2187 41.3125 53.125 40.8438 52.5Z" fill="white"/>
